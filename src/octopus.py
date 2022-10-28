e50bb0bc56cc83284fb556be2490a3e8a9d1d1f4
Paul Grave <paul.grave@sky.uk>
2022-10-28 18:09:28 +0100

    refactored: a class started to make sense

--- src/octopus.py
+++ src/octopus.py
@@ -3,27 +3,45 @@ import argparse
 import sys
 import json
 
-def concourse_in(data):
-    pass
+class ResourceType:
+    def __init__(self, data):
+        self.api_key = data['source']['api_key']
+        self.space_id = data['source']['space_id']
+        self.octopus_server_uri = data['source']['octopus_server_uri']
+        self.project_id = data['source']['project_id']
+        self.version = data.get('version').get('DeploymentId')
 
-def concourse_out(data):
-    pass
 
-def concourse_check(data):
-    api_key = data['source']['api_key']
-    space_id = data['source']['space_id']
-    octopus_server_uri = data['source']['octopus_server_uri']
-    project_id = data['source']['project_id']
+    def concourse_in(data):
+        pass
 
-    assert (space_id != None)
-    url = f"{octopus_server_uri}/api/{space_id}/deployments?projects={project_id}&taskState=Success"
-    req = requests.Request('GET', url, headers={ "X-Octopus-ApiKey": api_key})
-    prepped = req.prepare()
-    s = requests.Session()
-    response = s.send(prepped)
-    items = [ {"DeploymentId": i['Id'], "WebRef": f"{octopus_server_uri}{i['Links']['Web']}"} for i in response.json()['Items']]
-    print(json.dumps(items))
+    def concourse_out(data):
+        pass
 
+    def concourse_check(self):
+        take = 30
+        if not self.version:
+            take = 1
+
+        url = f"{self.octopus_server_uri}/api/{self.space_id}/deployments?projects={self.project_id}&taskState=Success&take={take}"
+        req = requests.Request('GET', url, headers={ "X-Octopus-ApiKey": self.api_key})
+        prepped = req.prepare()
+        s = requests.Session()
+        response = s.send(prepped)
+        items = self.latest_deployments_since_deploymentid(response.json(), self.version)
+        print(json.dumps(items))
+
+    def latest_deployments_since_deploymentid(self, deployments, deploymentid):
+        result = []
+        for deployment in deployments['Items']:
+            result.append(
+                {   "DeploymentId":     deployment['Id'],
+                    "WebRef":           f"{self.octopus_server_uri}{deployment['Links']['Web']}"
+                }
+            )
+            if deploymentid == deployment['Id']:
+                break
+        return result
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process some integers.')
@@ -33,10 +51,11 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     datain=json.load(args.input_file)
+    resourceType = ResourceType(datain)
 
     if args.function == "check":
-        concourse_check(datain)
+        resourceType.concourse_check()
     elif args.function == "out":
-        concourse_out(datain)
+        resourceType.concourse_out()
     else:
-        concourse_in(datain)
+        resourceType.concourse_in()
